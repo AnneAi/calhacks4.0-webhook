413ff286631ba1dd5899e1a7670f8d582af9ec64
Baboo7 <baptiste.studer@laposte.net>
2017-10-11 10:49:08 -0700

    fix/exercises wording

--- database/additions/exercises.js
+++ database/additions/exercises.js
@@ -1,19 +1,19 @@
 const exercises = [
   {
     level: 1,
-    wording: '11+8?',
+    wording: 'What is 11+8?',
     choices: [ '23', '19' ,'14'],
     answer: 1
   },
   {
     level: 2,
-    wording: '21+12?',
+    wording: 'What is 21+12?',
     choices: [ '33', '32' ,'34'],
     answer: 0
   },
   {
     level: 3,
-    wording: '228+191?',
+    wording: 'What is 228+191?',
     choices: [ '400', '411' ,'312'],
     answer: 1
   }
--- database/subtractions/exercises.js
+++ database/subtractions/exercises.js
@@ -1,19 +1,19 @@
 const exercises = [
   {
     level: 1,
-    wording: '7-3',
+    wording: 'What is 7-3?',
     choices: [ '4', '3', '7', '37' ],
     answer: 0
   },
   {
     level: 2,
-    wording: '20-8',
+    wording: 'What is 20-8?',
     choices: [ '12', '6', '11', '8' ],
     answer: 0
   },
   {
     level: 3,
-    wording: '22-18',
+    wording: 'What is 22-18?',
     choices: [ '4', '2', '3', '8' ],
     answer: 0
   }
